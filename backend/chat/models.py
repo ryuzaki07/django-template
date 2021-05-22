5cfac06c8e8e5992dbac56cac48e9e18bdd1ff56
ryuzaki07 <rahulnair.rn34@gmail.com>
2021-05-22 13:52:45 +0530

    another edit

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-#just here to some changes
+# just here to some changes
+# another change
+
 
 class Chat(models.Model):
+    test = models.CharField(max_length=100)
     chat = models.CharField(max_length=50)
     user = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True, blank=True)
